fbc62ea3f69c015498ab20fc05bfd1b2e33d6f69
John-George <johngeorgesample@gmail.com>
2017-03-20 18:49:30 -0400

    Created StackWrapper, created data.txt

---
+++ StackWrapper.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+public class StackWrapper<DT> { 
+	LinkedList<DT> L = new LinkedList(); 
+	StackWrapper() {
+		
+	}
+	
+	public void push(DT elem) {
+		L.addLast(elem);
+	}
+	
+	public DT pop() {
+		return L.removeLast();
+	}
+	
+	public DT peek() {
+		return L.peekLast();
+	}
+	
+	public boolean isEmpty() {
+		if(L.peekFirst() == null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		String bString = "";
+		for(int i = 0; i < L.size(); i++) {
+			bString += L.get(i);
+		}
+		return bString;
+	}
+  
+}
